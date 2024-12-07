ce055877583ef2fefd01da9619db77bbb2aca35b
Jack Rosenthal <jack@rosenth.al>
2024-12-06 22:00:10 -0700

    Initialize day 7

---
+++ 7/solution.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+}
