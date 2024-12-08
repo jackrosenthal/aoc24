b014587a4e0138239751f0d53dabdd6e6a4c8532
Jack Rosenthal <jack@rosenth.al>
2024-12-07 22:19:25 -0700

    part 1

--- 8/solution.go
+++ 8/solution.go
@@ -12,14 +12,63 @@ func check(e error) {
 	}
 }
 
+type Pos struct {
+	Row int
+	Col int
+}
+
+func findAntinodes(pos Pos, city []string, antinodes map[Pos]bool) {
+	channel := city[pos.Row][pos.Col]
+	for r, row := range city {
+		for c, cell := range row {
+			cellPos := Pos{r, c}
+			if cellPos == pos {
+				continue
+			}
+			if cell == rune(channel) {
+				dr := pos.Row - r
+				dc := pos.Col - c
+				posToAdd := []Pos{
+					{r - dr, c - dc},
+					{r + dr, c + dc},
+					{pos.Row + dr, pos.Col + dc},
+					{pos.Row - dr, pos.Col - dc},
+				}
+				for _, p := range posToAdd {
+					if p.Row < 0 || p.Row >= len(city) || p.Col < 0 || p.Col >= len(city[0]) {
+						continue
+					}
+					if p == pos || p == cellPos {
+						continue
+					}
+					antinodes[p] = true
+				}
+			}
+		}
+	}
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	check(err)
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	city := []string{}
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		city = append(city, line)
 	}
+
+	antinodes := map[Pos]bool{}
+	for r, row := range city {
+		for c, cell := range row {
+			if string(cell) == "." {
+				continue
+			}
+			findAntinodes(Pos{r, c}, city, antinodes)
+		}
+	}
+
+	fmt.Println(len(antinodes))
 }
