45d7c680e1c24a451be0a5245e36550248fd37a0
Jack Rosenthal <jack@rosenth.al>
2024-12-18 22:10:03 -0700

    Part 1

--- 19/solution.go
+++ 19/solution.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func check(e error) {
@@ -12,14 +13,54 @@ func check(e error) {
 	}
 }
 
+func designIsPossible(design string, towels map[byte][]string) bool {
+	if len(design) == 0 {
+		return true
+	}
+
+	options := []string{}
+	for _, towel := range towels[design[0]] {
+		if strings.HasPrefix(design, towel) {
+			options = append(options, towel)
+		}
+	}
+
+	for _, option := range options {
+		if designIsPossible(design[len(option):], towels) {
+			return true
+		}
+	}
+
+	return false
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	check(err)
 	defer file.Close()
 
+	towels := map[byte][]string{}
+	designs := []string{}
+
 	scanner := bufio.NewScanner(file)
+
+	scanner.Scan()
+	for _, towel := range strings.Split(scanner.Text(), ", ") {
+		towels[towel[0]] = append(towels[towel[0]], towel)
+	}
+
+	scanner.Scan()
+
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		designs = append(designs, line)
 	}
+
+	part1 := 0
+	for _, design := range designs {
+		if designIsPossible(design, towels) {
+			part1++
+		}
+	}
+	fmt.Println(part1)
 }
