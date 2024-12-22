1c7b16af5c1e28c4d900dc662cb40dd302bc68bc
Jack Rosenthal <jack@rosenth.al>
2024-12-21 22:40:28 -0700

    Part 2

--- 22/solution.go
+++ 22/solution.go
@@ -30,18 +30,65 @@ func simPrng(secret int, times int) int {
 	return secret
 }
 
+type Sequence struct {
+	a, b, c, d int
+}
+
+func (s Sequence) Push(val int) Sequence {
+	return Sequence{s.b, s.c, s.d, val}
+}
+
+func getSequences(secret int) map[Sequence]int {
+	result := map[Sequence]int{}
+	sequence := Sequence{0, 0, 0, 0}
+	lastPrice := 0
+	for i := 0; i < 2000; i++ {
+		secret = simPrng(secret, 1)
+		price := secret % 10
+		priceDelta := price - lastPrice
+		sequence = sequence.Push(priceDelta)
+		if i >= 4 {
+			if _, ok := result[sequence]; !ok {
+				result[sequence] = price
+			}
+		}
+		lastPrice = price
+	}
+	return result
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	check(err)
 	defer file.Close()
 
 	part1 := 0
+	sequences := []map[Sequence]int{}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 		num, err := strconv.Atoi(line)
 		check(err)
 		part1 += simPrng(num, 2000)
+		sequences = append(sequences, getSequences(num))
 	}
 	fmt.Println(part1)
+
+	allSequences := map[Sequence]bool{}
+	for _, seqs := range sequences {
+		for seq := range seqs {
+			allSequences[seq] = true
+		}
+	}
+
+	part2 := 0
+	for seq := range allSequences {
+		totalBananas := 0
+		for _, seqs := range sequences {
+			totalBananas += seqs[seq]
+		}
+		part2 = max(totalBananas, part2)
+	}
+
+	fmt.Println(part2)
 }
