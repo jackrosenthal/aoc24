60096854305e5e8c788819eae01617e69d1fcc81
Jack Rosenthal <jack@rosenth.al>
2024-12-21 22:14:59 -0700

    Part 1

--- 22/solution.go
+++ 22/solution.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func check(e error) {
@@ -12,14 +13,35 @@ func check(e error) {
 	}
 }
 
+func pruneSecret(secret int) int {
+	return secret % 16777216
+}
+
+func mixSecret(a int, b int) int {
+	return pruneSecret(a ^ b)
+}
+
+func simPrng(secret int, times int) int {
+	for i := 0; i < times; i++ {
+		secret = mixSecret(secret, secret*64)
+		secret = mixSecret(secret, secret/32)
+		secret = mixSecret(secret, secret*2048)
+	}
+	return secret
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	check(err)
 	defer file.Close()
 
+	part1 := 0
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		fmt.Println(line)
+		num, err := strconv.Atoi(line)
+		check(err)
+		part1 += simPrng(num, 2000)
 	}
+	fmt.Println(part1)
 }
