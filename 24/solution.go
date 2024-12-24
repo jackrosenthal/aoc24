dfb0a2ff978fb9cdb394257570cbc7ee4e6432ed
Jack Rosenthal <jack@rosenth.al>
2024-12-24 13:17:32 -0700

    Part 2

--- 24/solution.go
+++ 24/solution.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -63,6 +64,38 @@ func boolToInt(val bool) int {
 	return 0
 }
 
+func varName(letter rune, i int) string {
+	return fmt.Sprintf("%c%02d", letter, i)
+}
+
+func zToInt(varValues map[string]bool, varExprs map[string]Expr) int {
+	result := 0
+	for i := 0; i < 46; i++ {
+		varName := varName('z', i)
+		result |= boolToInt(eval(varName, varValues, varExprs)) << i
+	}
+	return result
+}
+
+func evalInt(x int, y int, varExprs map[string]Expr) int {
+	varValues := map[string]bool{}
+	for i := 0; i < 45; i++ {
+		varValues[varName('x', i)] = (x & (1 << i)) != 0
+		varValues[varName('y', i)] = (y & (1 << i)) != 0
+	}
+	return zToInt(varValues, varExprs)
+}
+
+func getBits(x int) []int {
+	bits := []int{}
+	for i := 0; i < 46; i++ {
+		if (x & (1 << i)) != 0 {
+			bits = append(bits, i)
+		}
+	}
+	return bits
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	check(err)
@@ -95,16 +128,33 @@ func main() {
 		}
 	}
 
-	part1 := 0
-	for i := 0; i < 100; i++ {
-		varName := fmt.Sprintf("z%02d", i)
-		_, hasValue := varValues[varName]
-		_, hasExpr := varExprs[varName]
-		if !hasValue && !hasExpr {
-			break
-		}
-		part1 |= boolToInt(eval(varName, varValues, varExprs)) << i
+	fmt.Println(zToInt(varValues, varExprs))
+
+	swaps := []string{}
+	swap := func(a, b string) {
+		varExprs[a], varExprs[b] = varExprs[b], varExprs[a]
+		swaps = append(swaps, a)
+		swaps = append(swaps, b)
 	}
 
-	fmt.Println(part1)
+	// Fixes are added here manually
+	swap("z07", "gmt")
+	swap("cbj", "qjj")
+	swap("dmn", "z18")
+	swap("cfk", "z35")
+
+	for i := 0; i < 45; i++ {
+		if result := evalInt(1<<i, 0, varExprs); result != 1<<i {
+			fmt.Printf("X=b%02d, Y=0 -> %v\n", i, getBits(result))
+		}
+		if result := evalInt(0, 1<<i, varExprs); result != 1<<i {
+			fmt.Printf("X=0, Y=b%02d -> %v\n", i, getBits(result))
+		}
+		if result := evalInt(1<<i, 1<<i, varExprs); result != 1<<(i+1) {
+			fmt.Printf("X=b%02d, Y=b%02d -> %v\n", i, i, getBits(result))
+		}
+	}
+
+	sort.Strings(swaps)
+	fmt.Println(strings.Join(swaps, ","))
 }
